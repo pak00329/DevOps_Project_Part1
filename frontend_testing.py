5a92ea24c2206bd8d579dd75e05ad05c00c46771
Kwame Gyimah <kwamegyimah@Kwames-MacBook-Pro.local>
2024-07-21 20:56:28 +0100

    Added Selenium Code for frontend testing

--- frontend_testing.py
+++ frontend_testing.py
@@ -1,1 +1,22 @@
-print('To get the output of the frontend')
+from selenium import webdriver
+
+# Start a Selenium Webdriver session
+driver = webdriver.Chrome()
+
+try:
+    # Navigate to web interface URL using an existing user id
+    driver.get("http://127.0.0.1:5000/user/3")
+
+    # Check that the user name element is showing (web element exists)
+    user_name_element = driver.find_element_by_css_selector(".user-name")
+
+    if user_name_element:
+        # Print user name
+        print("User Name:", user_name_element.text)
+    else:
+        print("User name element not found.")
+except Exception as e:
+    print("An error occurred:", e)
+finally:
+    # Close the browser session
+    driver.quit()
