31dff11875e16649ef4bb000bcd25f293f18c005
GyeeTech <73099283+GyeeTech@users.noreply.github.com>
2024-07-21 21:00:31 +0100

    Update frontend_testing.py
    Changed the port from 5000 to 5001

--- frontend_testing.py
+++ frontend_testing.py
@@ -5,7 +5,7 @@ driver = webdriver.Chrome()
 
 try:
     # Navigate to web interface URL using an existing user id
-    driver.get("http://127.0.0.1:5000/user/3")
+    driver.get("http://127.0.0.1:50001/user/3")
 
     # Check that the user name element is showing (web element exists)
     user_name_element = driver.find_element_by_css_selector(".user-name")
