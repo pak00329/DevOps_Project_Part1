c6ea2efd0193f6bacbd4e2b154f13ca12533066f
GyeeTech <73099283+GyeeTech@users.noreply.github.com>
2024-07-21 21:20:31 +0100

    Update clean_environment.py

--- clean_environment.py
+++ clean_environment.py
@@ -1,10 +1,10 @@
 import requests
 
-requests.get('http://127.0.0.1:5000/stop_server')
+requests.get('http://127.0.0.1:5001/stop_server')
 requests.get('http://127.0.0.1:5001/stop_server')
 
 try:
-    response = requests.get('http://127.0.0.1:5000/stop_server')
+    response = requests.get('http://127.0.0.1:5001/stop_server')
     response.raise_for_status()  # Raise an exception for 4xx/5xx status codes
     print(response.text)  # If needed, print the response text
 except requests.exceptions.RequestException as e:
